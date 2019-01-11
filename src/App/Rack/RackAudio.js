5b08207500962b0552a4b97a770186f15f57b73e
Bede Kelly <bede.kelly@accenture.com>
2019-01-11 02:01:10 +0000

    Refactor: Replace effectaudio switch statement with an object lookup

--- src/App/Rack/RackAudio.js
+++ src/App/Rack/RackAudio.js
@@ -17,49 +17,49 @@ class RackAudio {
         // Todo: global fx rack
         this.destination = this.graph.context.destination;
     }
-    
+
     pause() {
         this.source.pause();
     }
-    
+
     play() {
         this.source.play();
     }
-    
+
     removeEffect(id) {
         // Find and connect the old input and output of this effect.
         const effectIndex = this.effects.findIndex(effect => effect.id === id);
         const oldInput = this.inputOf(effectIndex);
         const oldOutput = this.outputOf(effectIndex);
         oldInput.routeTo(oldOutput);
-        
+
         // Clean up the effect by disconnecting it.
         const effect = this.effects[effectIndex];
         effect.disconnect();
-        
+
         // Remove the effect from this rack's list.
         this.effects.splice(effectIndex, 1);
     }
-    
+
     outputOf(index) {
         return (index >= this.effects.length-1) ? 
             this.destination : this.effects[index+1];
     }
-    
+
     inputOf(index) {
         return (index > 0) ? 
             this.effects[index-1] : this.source;
     }
-    
+
     moveEffect({oldIndex, newIndex}) {
         // A -> E -> B becomes A -> B.
         const oldOutput = this.outputOf(oldIndex);
         const oldInput = this.inputOf(oldIndex);
         const effect = this.effects[oldIndex];
         oldInput.routeTo(oldOutput);
-        
+
         // Transform our effects array to match the components.
-        this.effects = arrayMove(this.effects, oldIndex, newIndex)
+        this.effects = arrayMove(this.effects, oldIndex, newIndex);
 
         // C -> D becomes C -> E -> D.
         const newInput = this.inputOf(newIndex);
@@ -67,7 +67,7 @@ class RackAudio {
         newInput.routeTo(effect);
         effect.routeTo(newOutput);
     }
-    
+
     addSource(sourceType) {
         const sourceAudios = { [SourceType.TapeLooper]: TapeLooperAudio };
         const defaultSourceAudio = TapeLooperAudio;
@@ -77,39 +77,35 @@ class RackAudio {
         this.graph.sources[id] = this.source;
         return id;
     }
-    
+
     get startOfFxChain() {
         return (this.effects[0] && this.effects[0].node) || this.destination;
     }
-    
+
     get currentOutput() {
         return this.effects[this.effects.length-1] || this.source;
     }
-    
+
     addEffect(effectType) {
-        let effect;
-        const lastOutput = this.currentOutput;
-        switch(effectType) {
-            case EffectType.Pan: {
-                effect = new PanAudio(this);
-                this.effects.push(effect);
-                break;
-            }
-            case EffectType.Filter:
-            default: {
-                effect = new FilterAudio(this);
-                this.effects.push(effect);
-            }
-        }
+        // Make an EffectAudio and add it to this rack's effect.
+        const effectAudios = {
+            [EffectType.Pan]: PanAudio,
+            [EffectType.Filter]: FilterAudio
+        };
+        const defaultEffectAudio = FilterAudio;
+        const EffectAudio = effectAudios[effectType] || defaultEffectAudio
+        let effect = new EffectAudio(this)
+        this.effects.push(effect);
+
+        // Add an ID to register this effect.
         const id = uuid();
         this.graph.effects[id] = effect;
         effect.id = id;
-        
-        if (lastOutput) {
-            lastOutput.routeTo(effect);
-        }
+
+        // Route the current output to this effect.
+        const lastOutput = this.currentOutput;
+        if (lastOutput) lastOutput.routeTo(effect);
         effect.routeTo(this.destination);
-        
         return id;
     }
 }
