f6f9a50319258da1ac8575e068040bb88d2214ce
Bede Kelly <bxk561@student.bham.ac.uk>
2019-01-30 04:56:24 +0000

    className, htmlFor and lfo input fix

--- src/App/Sources/Synth/Synth.js
+++ src/App/Sources/Synth/Synth.js
@@ -7,162 +7,162 @@ class Synth extends React.Component {
     
     render() {
         return (
-            <div class="synth">
-              <div class="comp osc one">
-                <div class="knobs">
-                  <div class="knob"></div>
-                  <div class="label">Oct</div>
-                  <div class="knob"></div>
-                  <div class="label">Semi</div>
-                  <div class="knob"></div>
-                  <div class="label">Tune</div>
+            <div className="synth">
+              <div className="comp osc one">
+                <div className="knobs">
+                  <div className="knob"></div>
+                  <div className="label">Oct</div>
+                  <div className="knob"></div>
+                  <div className="label">Semi</div>
+                  <div className="knob"></div>
+                  <div className="label">Tune</div>
                 </div>
 
-                <div class="waveform">
-                  <label for="sine">
+                <div className="waveform">
+                  <label htmlFor="sine">
                     <input type="radio" name="waveform" value="sine" id="sine"></input>
-                    <div class="light"></div>
+                    <div className="light"></div>
                     <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/1415658/sinepro.svg' alt=''></img>
                   </label>
                 </div>
 
-                <div class="waveform">
-                  <label for="sawtooth">
+                <div className="waveform">
+                  <label htmlFor="sawtooth">
                     <input type="radio" name="waveform" value="sawtooth" id="sawtooth"></input>
-                    <div class="light"></div>
+                    <div className="light"></div>
                     <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/1415658/sawtoothpro.svg' alt=''></img>
                   </label>
                 </div>
 
-                <div class="waveform">
-                  <label for="triangle">
+                <div className="waveform">
+                  <label htmlFor="triangle">
                     <input type="radio" name="waveform" value="triangle" id="triangle"></input>
-                    <div class="light"></div>
+                    <div className="light"></div>
                     <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/1415658/trianglepro.svg' alt=''></img>
                   </label>
                 </div>
 
-                <div class="waveform square">
-                  <label for="square">
+                <div className="waveform square">
+                  <label htmlFor="square">
                     <input type="radio" name="waveform" value="square" id="square"></input>
-                    <div class="light"></div>
+                    <div className="light"></div>
                     <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/1415658/squarepro.svg' alt=''></img>
                   </label>
                 </div>
 
               </div>
-              <div class="comp osc two">
-                <div class="knobs">
-                  <div class="knob"></div>
-                  <div class="label">Oct</div>
-                  <div class="knob"></div>
-                  <div class="label">Semi</div>
-                  <div class="knob"></div>
-                  <div class="label">Tune</div>
+              <div className="comp osc two">
+                <div className="knobs">
+                  <div className="knob"></div>
+                  <div className="label">Oct</div>
+                  <div className="knob"></div>
+                  <div className="label">Semi</div>
+                  <div className="knob"></div>
+                  <div className="label">Tune</div>
                 </div>
 
-                <div class="waveform">
-                  <label for="sine2">
+                <div className="waveform">
+                  <label htmlFor="sine2">
                     <input type="radio" name="waveform-two" value="sine2" id="sine2"></input>
-                    <div class="light"></div>
+                    <div className="light"></div>
                     <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/1415658/sinepro.svg' alt=''></img>
                   </label>
                 </div>
 
-                <div class="waveform">
-                  <label for="sawtooth2">
+                <div className="waveform">
+                  <label htmlFor="sawtooth2">
                     <input type="radio" name="waveform-two" value="sawtooth2" id="sawtooth2"></input>
-                    <div class="light"></div>
+                    <div className="light"></div>
                     <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/1415658/sawtoothpro.svg' alt=''></img>
                   </label>
                 </div>
 
-                <div class="waveform">
-                  <label for="triangle2">
+                <div className="waveform">
+                  <label htmlFor="triangle2">
                     <input type="radio" name="waveform-two" value="triangle2" id="triangle2"></input>
-                    <div class="light"></div>
+                    <div className="light"></div>
                     <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/1415658/trianglepro.svg' alt=''></img>
                   </label>
                 </div>
 
-                <div class="waveform square">
-                  <label for="square2">
+                <div className="waveform square">
+                  <label htmlFor="square2">
                     <input type="radio" name="waveform-two" value="square2" id="square2"></input>
-                    <div class="light"></div>
+                    <div className="light"></div>
                     <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/1415658/squarepro.svg' alt=''></img>
                   </label>
                 </div>
 
               </div>
-              <div class="comp filter-freq">
-                <h2>Filter <span class="filter-type">HP</span></h2>
-                <div class="knobs">
-                  <div class="knob freq"></div>
-                  <div class="label freq">Freq</div>
-                  <div class="knob res"></div>
-                  <div class="label res">Res</div>
+              <div className="comp filter-freq">
+                <h2>Filter <span className="filter-type">HP</span></h2>
+                <div className="knobs">
+                  <div className="knob freq"></div>
+                  <div className="label freq">Freq</div>
+                  <div className="knob res"></div>
+                  <div className="label res">Res</div>
                 </div>
               </div>
-              <div class="comp envelope filter-envelope">
+              <div className="comp envelope filter-envelope">
                 <h2>Filter Env.</h2>
-                <div class="slidecontainer">
-                  <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+                <div className="slidecontainer">
+                  <input type="range" min="1" max="100" value="50" className="slider" id="myRange"></input>
                 </div>
-                <div class="slidecontainer">
-                  <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+                <div className="slidecontainer">
+                  <input type="range" min="1" max="100" value="50" className="slider" id="myRange"></input>
                 </div>
-                <div class="slidecontainer">
-                  <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+                <div className="slidecontainer">
+                  <input type="range" min="1" max="100" value="50" className="slider" id="myRange"></input>
                 </div>
-                <div class="slidecontainer">
-                  <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+                <div className="slidecontainer">
+                  <input type="range" min="1" max="100" value="50" className="slider" id="myRange"></input>
                 </div>
-                <div class="label">A</div>
-                <div class="label">D</div>
-                <div class="label">S</div>
-                <div class="label">R</div>
+                <div className="label">A</div>
+                <div className="label">D</div>
+                <div className="label">S</div>
+                <div className="label">R</div>
               </div>
-              <div class="comp lfo">
+              <div className="comp lfo">
                 <h2>LFO</h2>
-                <div class="lfo-layout">
-                  <div class="knob"></div>
-                  <div class="label">Rate</div>
-                  <div class="destinations">
-                    <div class="destination">
-                      <label for="pitch">
+                <div className="lfo-layout">
+                  <div className="knob"></div>
+                  <div className="label">Rate</div>
+                  <div className="destinations">
+                    <div className="destination">
+                      <label htmlFor="pitch">
                         <input type="radio" name="destination" value="destination" id="pitch"></input>
-                        <div class="light"></div>
-                        <span class="label">Pitch</span>
+                        <div className="light"></div>
+                        <span className="label">Pitch</span>
                       </label>
                     </div>
-                    <div class="destination">
-                      <label for="envelope amplitude-envelope">
+                    <div className="destination">
+                      <label htmlFor="amplitude">
                         <input type="radio" name="destination" value="destination" id="amplitude"></input>
-                        <div class="light"></div>
-                        <span class="label">Amp.</span>
+                        <div className="light"></div>
+                        <span className="label">Amp.</span>
                       </label>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="comp amp envelope">
+              <div className="comp amp envelope">
                 <h2>Amp Env.</h2>
-                <div class="slidecontainer">
-                  <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+                <div className="slidecontainer">
+                  <input type="range" min="1" max="100" value="50" className="slider" id="myRange"></input>
                 </div>
-                <div class="slidecontainer">
-                  <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+                <div className="slidecontainer">
+                  <input type="range" min="1" max="100" value="50" className="slider" id="myRange"></input>
                 </div>
-                <div class="slidecontainer">
-                  <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+                <div className="slidecontainer">
+                  <input type="range" min="1" max="100" value="50" className="slider" id="myRange"></input>
                 </div>
-                <div class="slidecontainer">
-                  <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+                <div className="slidecontainer">
+                  <input type="range" min="1" max="100" value="50" className="slider" id="myRange"></input>
                 </div>
-                <div class="label">A</div>
-                <div class="label">D</div>
-                <div class="label">S</div>
-                <div class="label">R</div>
+                <div className="label">A</div>
+                <div className="label">D</div>
+                <div className="label">S</div>
+                <div className="label">R</div>
               </div>
             </div>
         );
