e59d2a2e694ae1cee43cec89cbbe2de726a7ba30
Bede Kelly <bxk561@student.bham.ac.uk>
2019-02-21 00:54:53 +0000

    Working ambience, stock photos

--- src/App/Sources/Ambience/Ambience.js
+++ src/App/Sources/Ambience/Ambience.js
@@ -4,13 +4,31 @@ import './Ambience.css';
 
 
 class Ambience extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.audio = this.props.appAudio.sources[this.props.id];
+    }
+    
+    componentDidMount() {
+        this.audio.play();
+    }
+    
+    previous() {
+        this.audio.previous();
+    }
+    
+    next() {
+        this.audio.next();
+    }
+    
     render() {
         return <div className="ambience">
           <img src={"https://placeimg.com/300/200/any"} alt=""></img>
           <img className="blurred-image" src={"https://placeimg.com/300/200/any"} alt=""></img>
           <div className="inset-shadow"></div>
-          <div className="button left">{ "<" }</div>
-          <div className="button right">{ ">" }</div>
+          <div onClick={ () => this.previous() } className="button left">{ "<" }</div>
+          <div onClick={ () => this.next() } className="button right">{ ">" }</div>
         </div>;
     }
 }
--- src/App/Sources/Ambience/AmbienceAudio.js
+++ src/App/Sources/Ambience/AmbienceAudio.js
@@ -1,5 +1,131 @@
+const BUFFER_URL = name => 
+    `https://s3.eu-west-2.amazonaws.com/static-electricity/daw/ambience-sounds/${name.replace(" ", "+")}.wav`;
+const SCHEDULING_WINDOW = 4;
+const CROSSOVER = 2;
+
+
 export default class AmbienceAudio {
-    routeTo(output) {
-        console.log("Todo: route ambience audio to ", output);
+    
+    SOUNDS = ["Wind", "Record Crackle"]
+    
+    constructor(parentRack) {
+        this.soundBuffers = {};
+        this.parentRack = parentRack;
+        this.appAudio = parentRack.appAudio;
+        this.paused = true;
+        this.context = this.appAudio.context;
+        this.outputNode = this.context.createGain();
+        this.sourceNodes = [];
+        this.currentSound = 0;
+    }
+    
+    nextIndex() {
+        return (this.currentSound + 1) % this.SOUNDS.length;
+    }
+    
+    previousIndex() {
+        const newIndex = (this.currentSound - 1);
+        return newIndex < 0 ? newIndex + this.SOUNDS.length : newIndex;
+    }
+
+    next() {
+        this.cleanup({ allSounds: true });
+        this.currentSound = this.nextIndex();
+        this.play();
+        return this.SOUNDS[this.currentSound];
+    }
+
+    previous() {
+        this.cleanup({ allSounds: true });
+        this.currentSound = this.previousIndex();
+        this.play();
+        return this.SOUNDS[this.currentSound];
+    }
+    
+    async fetchAndLoadAudio(location) {
+        try {
+            const response = await fetch(location);
+            const arrayBuffer = await response.arrayBuffer();
+            return await this.context.decodeAudioData(arrayBuffer);
+        } catch (e) {
+            console.warn(e);
+        }
+    }
+
+    get currentBuffer() {
+        return this.soundBuffers[this.currentSound];
+    }
+
+    async loadSoundBuffer(index) {
+        if (this.soundBuffers[index]) return;
+        const name = this.SOUNDS[index];
+        if (name === undefined) debugger;
+        const url = BUFFER_URL(name);
+        this.soundBuffers[index] = await this.fetchAndLoadAudio(url);
+    }
+
+    startScheduling() {
+        this.interval = setInterval(this.scheduleNextSound.bind(this), 2000);
+    }
+
+    shouldScheduleSound(now, start, length) {
+        return (now > (start + length - CROSSOVER - SCHEDULING_WINDOW));
+    }
+
+    scheduleNextSound() {
+        const now = this.context.currentTime;
+        const start = this.startTime;
+        const length = this.currentBuffer.duration;
+        if (this.shouldScheduleSound(now, start, length)) {
+            this.playAt(start + length - CROSSOVER);
+        }
+        this.cleanup({ allSounds: false });
+    }
+
+    cleanup({ allSounds }) {
+        this.sourceNodes = this.sourceNodes.filter(({sourceNode, gainNode, endTime}) => {
+            if (allSounds || this.context.currentTime > endTime) {
+                sourceNode.disconnect();
+                gainNode.disconnect();
+                return false;
+            }
+            else { 
+                return true;
+            }
+        })
+    }
+
+    playAt(time) {
+        const sourceNode = this.context.createBufferSource();
+        const buffer = sourceNode.buffer = this.currentBuffer;
+        sourceNode.start(time);
+        
+        const gainNode = this.context.createGain();
+        sourceNode.connect(gainNode);
+        gainNode.connect(this.outputNode);
+        
+        gainNode.gain.setValueAtTime(0, time);
+        gainNode.gain.setTargetAtTime(1, time, CROSSOVER / 3);
+        const endTime = time + buffer.duration;
+        gainNode.gain.setTargetAtTime(0, endTime - CROSSOVER, CROSSOVER / 3);
+        
+        this.startTime = time;
+        this.sourceNodes.push({sourceNode, gainNode, endTime});
+    }
+
+    async play() {
+        await this.loadSoundBuffer(this.currentSound);
+        this.playAt(0);
+        this.startScheduling();
+        for (let i=0; i<this.SOUNDS.length; i++) { this.loadSoundBuffer(i) };
+    }
+
+    routeTo(destination) {
+        if (destination.input) {
+            destination = destination.input;
+        }
+        this.outputNode.disconnect();
+        this.outputNode.connect(destination);
+        
     }
 }
