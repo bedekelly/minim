c5b9d18a7bca65f1579a737c84e734b53413f4da
Bede Kelly <bxk561@student.bham.ac.uk>
2019-02-24 13:10:50 +0000

    Add missing label to Pan

--- src/App/Effects/Pan/Pan.js
+++ src/App/Effects/Pan/Pan.js
@@ -24,6 +24,7 @@ class Pan extends React.Component {
             <Slider min={ -1 } max={ 1 } step={ 0.01 }
                 value={ this.state.value }
                 default={ 0 }
+                label="Pan"
                 onChange={ value => this.onChange(value) }
                 appAudio={ this.props.appAudio }
                 id={ this.props.id + "-pan" }
